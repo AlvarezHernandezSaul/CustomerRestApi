b882291d7fdc049ffdb3ca76315f21c123fa70fa
AlvarezHernandezSaul <al222110813@gmail.com>
2024-06-29 19:29:06 -0600

    Mi API

---
+++ src/main/java/edu/utvt/attendance/persistence/common/StatusType.java
@@ -0,0 +1,5 @@
+package edu.utvt.attendance.persistence.common;
+
+public enum StatusType {
+ON, OFF
+}
---
+++ src/main/java/edu/utvt/attendance/persistence/common/StudentType.java
@@ -0,0 +1,5 @@
+package edu.utvt.attendance.persistence.common;
+
+public enum StudentType {
+FULL_TIME, DUAL
+}
